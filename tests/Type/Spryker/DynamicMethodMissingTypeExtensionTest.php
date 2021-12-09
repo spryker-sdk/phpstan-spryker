56c68fc17929551b3becd5d58f7adbbe587e0224
dima_tsemma <dmitry.tsemma@spryker.com>
2021-12-09 16:53:31 +0200

    TE-10139: WIP commit

--- tests/Type/Spryker/DynamicMethodMissingTypeExtensionTest.php
+++ tests/Type/Spryker/DynamicMethodMissingTypeExtensionTest.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace PHPStan\Test\Rules\Spryker;
+namespace SprykerSdk\PHPStanSpryker\Test\Rules\Spryker;
 
 use PHPStan\Cache\Cache;
 use PHPStan\Reflection\Annotations\AnnotationsMethodsClassReflectionExtension;
